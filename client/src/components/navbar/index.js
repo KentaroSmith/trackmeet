ca5fcaf18fc7d5250fd3c51c6dd2cd460a85f0a5
Dan Smith <daniel.smith8795@gmail.com>
2019-10-29 20:20:11 -0500

    added in the calendar and reactstrap

---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+const NavigationBar = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+              </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+
+export default NavigationBar;
+/* function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="\">Welcome to Trackmeet!</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="/saved">Calendar</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar */
---
+++ client/src/pages/calendar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Navbar from '../components/navbar/index'
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+/* import 'react-big-calendar/lib/addons/dragAndDrop/styles'; */
+
+import moment from 'moment';
+const localizer = momentLocalizer(moment)
+class CalendarPage extends Component {
+    state = {
+        events: [
+            {
+                start: new Date(),
+                end: new Date(moment().add(1, "days")),
+                title: "Some title"
+            }
+        ]
+    };
+
+    render() {
+        return (
+            <div className="Calendardiv">
+                <Navbar />
+                <Calendar
+                    localizer={localizer}
+                    events={this.state.events}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+
+            </div>
+        );
+    }
+}
+
+export default CalendarPage;
