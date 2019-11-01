d0ac1d7136ffe0a9cc3f611f6849b1a10b9cad91
Dan Smith <daniel.smith8795@gmail.com>
2019-10-31 19:20:58 -0500

    Fixed the calendar style issue

--- client/src/pages/calendar.js
+++ client/src/pages/calendar.js
@@ -3,7 +3,7 @@ import Navbar from '../components/navbar/index'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 /* import 'react-big-calendar/lib/addons/dragAndDrop/styles'; */
-
+import './calendar.css'
 import moment from 'moment';
 const localizer = momentLocalizer(moment)
 class CalendarPage extends Component {
