cc98b3026e4743b4c3d4846f593f14122c9f9637
Dan Smith <daniel.smith8795@gmail.com>
2019-11-02 08:00:28 -0500

    made the room schema

---
+++ models/rooms.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    roomName: { type: String, required: true },
+    amenities: { type: Array, required: true },
+    building: { type: String, required: true },
+    occupancy: Number,
+    timeAvailable: String
+});
+
+const Room = mongoose.model("Room", roomSchema);
+
+module.exports = Room;
