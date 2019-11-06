2ca1fbb4f722016547dc8f02fcd9010a9cc59f13
Miles Standoffish <toddnan@gmail.com>
2019-11-05 19:19:12 -0600

    fixed merge problem with api.js

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 import { withRouter } from "react-router";
 import app from "../components/Firebase/firebase";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import API from "../utils/API";
+import API from "../utils/api";
 
 // save new User data (except for password) to the database
 const saveUserData = (firstName, lastName, phone, email) => {
