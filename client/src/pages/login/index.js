b4a402c281935861535ca609271718003b8cbccf
Miles Standoffish <toddnan@gmail.com>
2019-11-07 16:49:05 -0600

    styled the login and signup pages

---
+++ client/src/pages/login/index.js
@@ -0,0 +1,84 @@
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import { Link } from "react-router-dom";
+import app from "../../components/Firebase/firebase";
+import { AuthContext } from "../../components/Firebase/auth";
+import {
+    Button, Form, FormGroup, Input,
+    Card, CardImg, CardBody, CardTitle, CardText
+} from 'reactstrap';
+import API from "../../utils/api";
+import { useDispatch } from 'react-redux';
+import { updateUser } from "../../actions";
+import Navbar from '../../components/navbar/index';
+import "./style.css";
+import logo from '../../assets/trackmeet-logo.png';
+
+// save new User data (except for password) to the database
+const getUserData = (email, callback) => {
+    API.getUser(email)
+        .then(res => {
+            //console.log(res);
+            console.log(res.data[0]);
+            // now push the user data into global state
+            callback(res.data[0]);
+        })
+        .catch(err => console.log(err));
+}
+
+const Login = ({ history }) => {
+    const dispatch = useDispatch();
+
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault();
+            const { email, password } = event.target.elements;
+            try {
+                await app
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                getUserData(email.value, (user) => dispatch(updateUser(user)));
+                history.push("/confirm"); // this page loads on successful user login
+            } catch (error) {
+                alert(error);
+            }
+        },
+        [history]);
+
+    const { currentUser } = useContext(AuthContext);
+
+    if (currentUser) {
+        return <Redirect to="/confirm" />
+    }
+
+
+
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <Card className="mx-auto shadow-lg">
+                    <CardImg top width="100%" src={logo} alt="TrackMeet logo" />
+                    <CardBody>
+                        <CardTitle>Log in</CardTitle>
+                        <Form onSubmit={handleLogin}>
+                            <FormGroup>
+                                <Input name="email" type="email" placeholder="Email" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="password" type="password" placeholder="Password" />
+                            </FormGroup>
+                            <Button type="submit" className="btn-block">Log in</Button>
+                        </Form>
+                        <CardText>
+                            <small>New to TrackMeet? <Link to="/signup">Create an account</Link></small>
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
+    );
+
+};
+
+export default withRouter(Login);
