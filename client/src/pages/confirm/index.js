237998e829d820da260552037fef5d80cf6cc478
Miles Standoffish <toddnan@gmail.com>
2019-11-12 11:18:52 -0600

    /reservations and /confirm load user data to global state if needed

--- client/src/pages/confirm/index.js
+++ client/src/pages/confirm/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import app from "../../components/Firebase/firebase";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
     Button, Form, FormGroup, Input,
     Card, CardImg, CardBody, CardTitle, CardText
@@ -10,6 +10,8 @@ import {
 import Navbar from '../../components/navbar/index';
 import API from "../../utils/api";
 import "./style.css";
+import { AuthContext } from "../../components/Firebase/auth";
+import { updateUser } from "../../actions";
 
 const mongojs = require("mongojs");
 
@@ -33,42 +35,74 @@ const makeReservation = (user, room) => {
         })
         .catch(err => console.log(err));
 
+
 };
 
 const Confirm = () => {
+    const { currentUser } = useContext(AuthContext);
     const userData = useSelector(state => state.user);
     const roomData = useSelector(state => state.room);
+    const dispatch = useDispatch();
+    const [events, setEvents] = useState([]);
 
-    return (
-        <>
-            <Navbar />
-            <Card id="confirm-card" className="mx-auto shadow-lg">
-                <CardBody>
-                    <CardTitle>Reservation confirmed:</CardTitle>
-                    <p>Reserved by: {userData.firstName} {userData.lastName}</p>
-                    <p>Email: {userData.email}</p>
-                    <p>Phone: {userData.phone}</p>
-                    <p>Location: {roomData.building}</p>
-                    <p>Room: {roomData.roomName}</p>
-                    <p>Max capacity: {roomData.occupancy}</p>
-                    <p>Features:</p>
-                    <ul>
-                        {!roomData.features ||
-                            roomData.features.map((feature, index) => (
-                                <li key={feature}>{feature}</li>
-                            ))}
-                    </ul>
-                    <p>Reservation period:</p>
-                    <p>Wednesday, Nov. 13</p>
-                    <p>6:00 - 8:00pm</p>
+    // loads once when the component mounts:
+    useEffect(
+        () => {
+            // load user info into global state (Redux) if we haven't already
+            getUserData(currentUser.email, (user) => {
+                dispatch(updateUser(user));
+                // load user data from database
+                API.getEventsByUser(user._id)
+                    .then(res => {
+                        setEvents(res.data);
+                    })
+                    .catch(err => console.log(err));
+            });
+        },
+        []);
 
-                    <Button onClick={() => makeReservation(userData, roomData)} className="btn-block">Reserve room</Button>
-                    <Button onClick={() => app.auth().signOut()} className="btn-block">Sign out</Button>
-                    <Link to="/reservations">Go to reservations</Link>  
-                </CardBody>
-            </Card>
-        </>
-    );
-};
+    const getUserData = (email, callback) => {
+        console.log("getting user for email: " + email);
+        API.getUser(email)
+            .then(res => {
+                //console.log(res);
+                console.log(res.data[0]);
+                // now push the user data into global state
+                callback(res.data[0]);
+            })
+            .catch(err => console.log(err));
+        }
 
-export default withRouter(Confirm);
+        return (
+            <>
+                <Navbar />
+                <Card id="confirm-card" className="mx-auto shadow-lg">
+                    <CardBody>
+                        <CardTitle>Reservation confirmed:</CardTitle>
+                        <p>Reserved by: {userData.firstName} {userData.lastName}</p>
+                        <p>Email: {userData.email}</p>
+                        <p>Phone: {userData.phone}</p>
+                        <p>Location: {roomData.building}</p>
+                        <p>Room: {roomData.roomName}</p>
+                        <p>Max capacity: {roomData.occupancy}</p>
+                        <p>Features:</p>
+                        <ul>
+                            {!roomData.features ||
+                                roomData.features.map((feature, index) => (
+                                    <li key={feature}>{feature}</li>
+                                ))}
+                        </ul>
+                        <p>Reservation period:</p>
+                        <p>Wednesday, Nov. 13</p>
+                        <p>6:00 - 8:00pm</p>
+
+                        <Button onClick={() => makeReservation(userData, roomData)} className="btn-block">Reserve room</Button>
+                        <Button onClick={() => app.auth().signOut()} className="btn-block">Sign out</Button>
+                        <Link to="/reservations">Go to reservations</Link>
+                    </CardBody>
+                </Card>
+            </>
+        );
+    };
+
+    export default withRouter(Confirm);
--- client/src/pages/reservations/index.js
+++ client/src/pages/reservations/index.js
@@ -1,33 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import app from "../../components/Firebase/firebase";
 import Reservation from "../../components/reservation/index";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
     Button,
     Container, Row, Col
 } from 'reactstrap';
 import Navbar from '../../components/navbar/index';
 import API from "../../utils/api";
+import { AuthContext } from "../../components/Firebase/auth";
+import { updateUser } from "../../actions";
 
 const Reservations = () => {
+    const { currentUser } = useContext(AuthContext);
     const user = useSelector(state => state.user);
     const [events, setEvents] = useState([]);
+    const dispatch = useDispatch();
 
-    console.log("the user is " + user.firstName)
+    console.log("the user is " + currentUser.email)
 
+    // loads once when the component mounts:
     useEffect(
         () => {
-            loadReservations();
-        }, 
+            // load user info into global state (Redux) if we haven't already
+            getUserData(currentUser.email, (user) => {
+                console.log(user);
+                dispatch(updateUser(user));
+                console.log(user);
+                // load user data from database
+                //loadReservations();
+                API.getEventsByUser(user._id)
+                    .then(res => {
+                        console.log(res.data);
+                        setEvents(res.data);
+                    })
+                    .catch(err => console.log(err));
+            });
+        },
         []);
 
+    const getUserData = (email, callback) => {
+        console.log("getting user for email: " + email);
+        API.getUser(email)
+            .then(res => {
+                //console.log(res);
+                console.log(res.data[0]);
+                // now push the user data into global state
+                callback(res.data[0]);
+            })
+            .catch(err => console.log(err));
+    }
+
     const loadReservations = () => {
+        console.log("loadReservations: " + user._id)
         API.getEventsByUser(user._id)
-        .then(res => {
-            console.log(res.data);
-            setEvents(res.data);
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                console.log(res.data);
+                setEvents(res.data);
+            })
+            .catch(err => console.log(err));
 
         /*API.getEvents()
             .then(res => {
