b28e6e8c783cd2a83d66f325d9d59136474e791e
Miles Standoffish <toddnan@gmail.com>
2020-06-09 10:43:25 -0500

    Modal to confirm location delete

--- client/src/components/LocationsList/index.js
+++ client/src/components/LocationsList/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { ListGroup, ListGroupItem, Collapse, Button, Container, Row, Col, Badge } from 'reactstrap';
+import React, { useState, useRef } from 'react';
+import { ListGroup, ListGroupItem, Collapse, Button, Container, Row, Col, Badge,
+    Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faPencilAlt as editIcon,
@@ -41,6 +42,10 @@ const Room = ({ room, onClick, onClickDelete }) => {
 
 
 const LocationsList = ({ locations, roomCounts, activeLocationId, roomsByLocation, onClickLocation, onClickAdd, onClickEdit, onClickDeleteLocation, onClickDelete, onClickRoom }) => {
+    const activeLoc = useRef(0); // used to capture the ID of the location to be deleted
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+    const toggleDeleteModal = () => setShowDeleteModal(!showDeleteModal);
 
     const roomCount = (locId) => {
         const result = roomCounts.find((loc) => loc._id === locId);
@@ -48,66 +53,91 @@ const LocationsList = ({ locations, roomCounts, activeLocationId, roomsByLocatio
     }
 
     return (
-        <ListGroup>
-            {locations.map((location) => (
-                <ListGroupItem
-                    key={location._id}
-                    tag="a"
-                    action
-                    onClick={() => onClickLocation(location._id)}
-                >
-                    <Container>
-                        <Row>
-                            <Col>{location.name} <Badge pill>{roomCount(location._id)}</Badge></Col>
-                            {activeLocationId === location._id
-                                ? <Col className="col-auto">
-                                    <Button
-                                        className="delete-btn fa-stack"
-                                        onClick={() => onClickDeleteLocation(location._id)}
-                                        style={{ marginRight: 10 }}
-                                        disabled={roomCount(location._id) > 0}
-                                    >
-                                        <FontAwesomeIcon icon={deleteIcon} className="fa-stack-1x" />
-                                        { (roomCount(location._id) > 0) &&
-                                            <FontAwesomeIcon icon={notAllowedIcon} className="not-sign fa-stack-2x" />
-                                        }
-                                    </Button>
-                                    <Button className="edit-btn" onClick={onClickEdit} style={{ marginRight: 15 }}>
-                                        <FontAwesomeIcon icon={editIcon} size="1x" />
-                                    </Button>
-                                    <Button className="add-btn" onClick={onClickAdd}>
-                                        <FontAwesomeIcon icon={addIcon} size="1x" />
-                                    </Button>
-                                </Col>
-                                : null}
-                        </Row>
-                    </Container>
+        <>
+            <ListGroup>
+                {locations.map((location) => (
+                    <ListGroupItem
+                        key={location._id}
+                        tag="a"
+                        action
+                        onClick={() => onClickLocation(location._id)}
+                    >
+                        <Container>
+                            <Row>
+                                <Col>{location.name} <Badge pill>{roomCount(location._id)}</Badge></Col>
+                                {activeLocationId === location._id
+                                    ? <Col className="col-auto">
+                                        <Button
+                                            className="delete-btn fa-stack"
+                                            onClick={() => {
+                                                activeLoc.current = location._id;
+                                                toggleDeleteModal();
+                                                //onClickDeleteLocation(location._id);
+                                            }}
+                                            style={{ marginRight: 10 }}
+                                            disabled={roomCount(location._id) > 0}
+                                        >
+                                            <FontAwesomeIcon icon={deleteIcon} className="fa-stack-1x" />
+                                            {(roomCount(location._id) > 0) &&
+                                                <FontAwesomeIcon icon={notAllowedIcon} className="not-sign fa-stack-2x" />
+                                            }
+                                        </Button>
+                                        <Button className="edit-btn" onClick={onClickEdit} style={{ marginRight: 15 }}>
+                                            <FontAwesomeIcon icon={editIcon} size="1x" />
+                                        </Button>
+                                        <Button className="add-btn" onClick={onClickAdd}>
+                                            <FontAwesomeIcon icon={addIcon} size="1x" />
+                                        </Button>
+                                    </Col>
+                                    : null}
+                            </Row>
+                        </Container>
 
-                    <Collapse isOpen={activeLocationId === location._id}>
-                        <ListGroup>
-                            {(roomCount(location._id) === 0)
-                                ? (<ListGroupItem>No rooms yet.</ListGroupItem>)
-                                : <>
-                                    {roomsByLocation.map((loc) => {
-                                        return (loc.locationId === location._id)
-                                            ? loc.rooms.map((room) => (
-                                                <Room
-                                                    key={room._id}
-                                                    room={room}
-                                                    onClick={() => onClickRoom(room._id)}
-                                                    onClickDelete={() => onClickDelete(room._id)}
-                                                />
-                                            ))
-                                            : null
-                                    })}
-                                </>
-                            }
-                        </ListGroup>
-                    </Collapse>
-                </ListGroupItem>
-            )
-            )}
-        </ListGroup>
+                        <Collapse isOpen={activeLocationId === location._id}>
+                            <ListGroup>
+                                {(roomCount(location._id) === 0)
+                                    ? (<ListGroupItem>No rooms yet.</ListGroupItem>)
+                                    : <>
+                                        {roomsByLocation.map((loc) => {
+                                            return (loc.locationId === location._id)
+                                                ? loc.rooms.map((room) => (
+                                                    <Room
+                                                        key={room._id}
+                                                        room={room}
+                                                        onClick={() => onClickRoom(room._id)}
+                                                        onClickDelete={() => onClickDelete(room._id)}
+                                                    />
+                                                ))
+                                                : null
+                                        })}
+                                    </>
+                                }
+                            </ListGroup>
+                        </Collapse>
+                    </ListGroupItem>
+                )
+                )}
+            </ListGroup>
+
+            <Modal isOpen={showDeleteModal} toggle={toggleDeleteModal} className="delete-modal">
+                <ModalHeader toggle={toggleDeleteModal}>
+                    Delete location?
+                </ModalHeader>
+                <ModalBody>
+                    Are you sure you want to delete <strong>{showDeleteModal && locations.find(loc => loc._id === activeLoc.current).name}</strong>?
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="danger"
+                        onClick={() => {
+                            onClickDeleteLocation(activeLoc.current);
+                            toggleDeleteModal();
+                        }}>
+                        Yes, delete it
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggleDeleteModal}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        </>
     );
 }
 
